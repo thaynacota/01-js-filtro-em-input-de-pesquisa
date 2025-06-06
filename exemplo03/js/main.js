4001929942a3302a479a00c6fa07e0c11d1677d7
Thayná da Silva Cota <thaynacota390@gmail.com>
2025-06-06 18:00:46 -0300

    Modificação das funções no main para que as ações editar e excluir sejam realizadas e arquivos desnecessários excluídos

--- exemplo03/js/main.js
+++ exemplo03/js/main.js
@@ -1,13 +1,13 @@
-import { enviar, buscar } from "./api/api.js";
+import { enviar, buscar, atualizar, remover } from "./api/api.js";
 import lerDadosDoFormulario from "./ler.js";
 import imprimirDadosDaLista from "./imprimir.js";
-import excluirItem from "./excluir.js";
-import editarItem from "./editar.js";
 
 const produto = document.getElementById("produto");
 const preco = document.getElementById("preco");
 const data_validade = document.getElementById("data");
 const botao = document.getElementById("botaoEnviar");
+
+//Variável criada para armazenar o estado do id (se esta sendo criado ou atualizado)
 let idEditando = null;
 
 window.addEventListener('DOMContentLoaded', async () => {
@@ -15,9 +15,30 @@ window.addEventListener('DOMContentLoaded', async () => {
     atualizarLista(vetor);
 });
 
-botao.addEventListener("click", (event) => {
+botao.addEventListener("click", async (event) => {
     event.preventDefault();
-    adicionar();
+    const dados = lerDadosDoFormulario();
+
+    //Verifica se todos os dados foram preenchidos
+    if (!dados) {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    //Atualiza um item já existente
+    if (idEditando) {
+        await atualizar(idEditando, dados);
+        idEditando = null; //reconfigura a variável (limpa o que esta nela)
+        botao.textContent = "Enviar";
+    }
+
+    //Adiciona novo item
+    else {
+        await enviar(dados);
+    }
+
+    const vetor = await buscar();
+    atualizarLista(vetor);
 });
 
 botao.addEventListener('keydown', (event) => {
@@ -25,24 +46,6 @@ botao.addEventListener('keydown', (event) => {
         adicionar();
     }
 });
-
-async function adicionar() {
-    const dados = lerDadosDoFormulario();
-    if (!dados) {
-        alert("Dados não enviados!");
-        return;
-    } 
-
-    if (idEditando) {
-        await atualizar(idEditando, dados);
-        idEditando = null;
-    } else {
-        await enviar(dados);
-    }
-
-    const vetor = await buscar();
-    atualizarLista(vetor);
-}
 
 function atualizarLista(vetor) {
     const lista = document.getElementById('lista');
@@ -52,36 +55,55 @@ function atualizarLista(vetor) {
         const li = document.createElement('li');
         li.innerHTML = imprimirDadosDaLista(elemento);
 
+
+        //Botão para editar todos os campos disponíveis
         const botaoEditar = document.createElement('button');
         botaoEditar.textContent = "Editar";
-        botaoEditar.onclick = () => editarItem(elemento);
+        botaoEditar.onclick = () => {
+            produto.value = elemento.produto;
+            preco.value = elemento.preco;
+            data_validade.value = elemento.data_validade;
+            idEditando = elemento.id;
+            botao.textContent = "Atualizar";
+        };
 
+        //Botão para excluir todos os campos disponíveis
         const botaoExcluir = document.createElement('button');
         botaoExcluir.textContent = "Excluir";
         botaoExcluir.onclick = async () => {
-            await remover(elemento.id);
-            const novaLista = await buscar();
-            atualizarLista(novaLista);
+            const confirmado = confirm("Tem certeza que deseja excluir?");
+            if (confirmado) {
+                await remover(elemento.id);
+                const novaLista = await buscar();
+                atualizarLista(novaLista);
+            }
         };
 
         li.appendChild(botaoEditar);
         li.appendChild(botaoExcluir);
-
         lista.appendChild(li);
     });
 }
 
-
 produto.onkeyup = () => {
-    var termo = produto.value.toLowerCase();
-    filtrar(termo)
+    const termo = produto.value.toLowerCase();
+
+    filtrar(termo);
 };
 
-preco.onkeyup = () => {
-    var termo = preco.value.toLowerCase();
-    filtrar(termo)
+preco.oninput = () => {
+    const termo = preco.value;
+
+    filtrar(termo);
 };
 
+data_validade.oninput = () => {
+    const termo = data_validade.value;
+
+    filtrar(termo);
+};
+
+//Preciso arrumar o filtro
 function filtrar(termo) {
     var itens = document.getElementsByTagName("li");
 
@@ -90,7 +112,10 @@ function filtrar(termo) {
 
         if (item.toLowerCase().includes(termo))
             itens[i].style.display = "flex";
+        else if (termo === "" || item === termo)
+            itens[i].style.display = "flex";
         else
             itens[i].style.display = "none";
     }
-};
+}
+
